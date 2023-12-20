253275635ccc9afcfdb816f35e6ad5306c07e05a
Bucky <75648240+Bucky-26@users.noreply.github.com>
2023-12-20 21:36:39 +0800

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,105 @@
+import express from 'express';
+import Bard from 'bard-ai';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { promises as fsPromises } from 'fs';
+import fetch from 'node-fetch';
+
+const app = express();
+const port = process.env.PORT || 80;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const token = '';
+// Middleware to parse JSON
+app.use(express.json());
+
+const jsonFilePath = join(__dirname, '/_conversation.json');
+const imgFolderPath = join(__dirname, '/image');
+
+// Ensure the 'img' folder exists
+fsPromises.mkdir(imgFolderPath, { recursive: true })
+	.catch(err => {
+		console.error('Error creating "img" folder:', err);
+	});
+
+async function updateUserData(userId, newData) {
+	try {
+		const fileContent = await fsPromises.readFile(jsonFilePath, 'utf-8');
+		const jsonData = JSON.parse(fileContent);
+
+		if (jsonData.hasOwnProperty(userId)) {
+			// Update the data for the existing user
+			jsonData[userId] = { ...jsonData[userId], ...newData };
+			console.log('Data updated successfully:', jsonData[userId]);
+		} else {
+			// Add new data for the user
+			jsonData[userId] = newData;
+			console.log('New data added successfully:', jsonData[userId]);
+		}
+
+		// Write the updated data back to the file
+		await fsPromises.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf-8');
+	} catch (error) {
+		console.error('Error retrieving or updating data in the JSON file:', error);
+	}
+}
+
+async function getUserDataById(userId) {
+	try {
+		const fileContent = await fsPromises.readFile(jsonFilePath, 'utf-8');
+		const jsonData = JSON.parse(fileContent);
+
+		return jsonData.hasOwnProperty(userId) ? jsonData[userId] : null;
+	} catch (error) {
+		throw new Error('Error retrieving data from JSON file: ' + error.message);
+	}
+}
+
+app.get('/api/bard/', async (req, res) => {
+	const _message = req.query.message;
+	const _id = req.query.id;
+	let _image_url = req.query.url;
+	const _api = req.query.api;
+
+	if (_api !== 'easy2023') {
+		return res.status(401).json({ error: "Unauthorized request invalid 'api'" });
+	} else if (!_message || !_id) {
+		return res.status(400).json({ error: "Required query parameters missing" });
+	}
+
+	try {
+		const bard = new Bard(token);
+		const _ids = await getUserDataById(_id);
+
+		if (_image_url) {
+			const imageResponse = await fetch(_image_url);
+			const imageBuffer = await imageResponse.buffer();
+			const imageName = `${_id}_image.jpg`;
+			const imagePath = join(imgFolderPath, imageName);
+
+			// Save the image to the 'img' folder
+			await fsPromises.writeFile(imagePath, imageBuffer);
+			_image_url = imagePath;
+		}
+
+		let myChat = bard.createChat(_ids);
+		const ress = await myChat.ask(_message, { image: _image_url, format: Bard.JSON });
+		let data = ress;
+		let content = data.content + "\n\nThis API is Develop And Maintain By EASY API\nIn EASY API we make it EASY";
+		let images = data.images.map(image => image.url);
+		res.json({
+			content,
+			images
+		});
+		const chat = await myChat.export();
+		await updateUserData(_id, chat);
+	} catch (error) {
+		console.error('Error calling Bard API:', error);
+		res.status(500).json({ error: 'Error calling Bard API' });
+	}
+});
+
+// Start the Express server
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
